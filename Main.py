c41c68a2a5b8e0fb62ace2793df6e953a9496d0f
Víctor Sevilla <sevillasanchez6@gmail.com>
2025-05-21 21:14:55 +0200

    Pasarle el log de error a chat

---
+++ Main.py
@@ -0,0 +1,9 @@
+import cv2
+import mediapipe as mp
+import time
+
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
